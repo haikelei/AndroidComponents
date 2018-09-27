38dc6bc4a02ce4891714cd7f512305a50828e0d4
lujialei <lujialei08@163.com>
2018-09-27 11:25:21 +0800

    增加网络库

--- app/src/main/java/components/luyuan/com/androidcomponents/MainActivity.java
+++ app/src/main/java/components/luyuan/com/androidcomponents/MainActivity.java
@@ -3,11 +3,30 @@ package components.luyuan.com.androidcomponents;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhouyou.http.EasyHttp;
+import com.zhouyou.http.callback.SimpleCallBack;
+import com.zhouyou.http.exception.ApiException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EasyHttp.post("api/")
+                .baseUrl("http://xxxx.xx.xx/dlydbg/")
+                .upJson("{\"\":\"\",\"\":\"\",\"\":\"\",\"swry_dm\":\"127053096\",\"version\":\"1.0.0\"}")
+                //这里不想解析，简单只是为了做演示 直接返回String
+                .execute(new SimpleCallBack<String>() {
+                    @Override
+                    public void onError(ApiException e) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(String s) {
+
+                    }
+                });
     }
 }
